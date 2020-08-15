d4db6f7fa7ae69184733b8202fbec85bc3f037c6
Gabriel Gamarra <gabrielgamarra2018@gmail.com>
2020-08-14 21:52:36 -0300

    agregado validacion al login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,35 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
+window.addEventListener('load', function() {
 });
+document.getElementById("btn1").addEventListener("click", function (e) {
+    if (document.getElementById("user").value.length != 0 && document.getElementById("pass").value.length != 0) {
+
+        return location.href = "index.html";
+    } else {
+        
+        if (document.getElementById("user").value.length == 0)
+        {
+            document.getElementById("user").style.borderColor = 'red'; // como se escribe en JS
+            document.getElementById("user").style.backgroundColor = 'pink';
+        } else
+        {
+            document.getElementById("user").style.removeProperty('border-color'); // Como se escribe en CSS
+            document.getElementById("user").style.removeProperty('background-color');
+        }
+
+        if (document.getElementById("pass").value.length == 0)
+        {
+            document.getElementById("pass").style.borderColor = 'red';
+            document.getElementById("pass").style.backgroundColor = 'pink';
+        } else
+        {
+            document.getElementById("pass").style.removeProperty('border-color'); // Como se escribe en CSS
+            document.getElementById("pass").style.removeProperty('background-color');
+        }
+    }
+});
+    
+
+
--- js/products.js
+++ js/products.js
@@ -1,81 +1,33 @@
-const ORDER_ASC_BY_NAME = "AZ";
-const ORDER_DESC_BY_NAME = "ZA";
-const ORDER_BY_PROD_COUNT = "Cant.";
-var currentCategoriesArray = [];
-var currentSortCriteria = undefined;
-var minCount = undefined;
-var maxCount = undefined;
+var currentProductArray = [];
 
-function sortCategories(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
-        result = array.sort(function(a, b) {
-            let aCount = parseInt(a.productCount);
-            let bCount = parseInt(b.productCount);
 
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
-            return 0;
-        });
-    }
-
-    return result;
-}
-
-function showCategoriesList(){
+function showProductList() {
 
     let htmlContentToAppend = "";
-    for(let i = 0; i < currentCategoriesArray.length; i++){
-        let category = currentCategoriesArray[i];
+    for (let i = 0; i < currentProductArray.length; i++) {
+        let product = currentProductArray[i];
 
-        if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
 
-            htmlContentToAppend += `
+        htmlContentToAppend += `
             <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ category.name +`</h4>
-                            <small class="text-muted">` + category.soldCount + ` artículos</small>
+                            <h4 class="mb-1">`+ product.name + `</h4>
+                            <small class="text-muted">` + product.soldCount + ` artículos</small>
                         </div>
-                        <p class="mb-1">` + category.description + `</p>
+                        <p class="mb-1">` + product.description + `</p>
                     </div>
                 </div>
             </a>
-            `
-        }
+            `;
+
 
         document.getElementById("product-list-container").innerHTML = htmlContentToAppend;
     }
-}
-function sortAndShowCategories(sortCriteria, categoriesArray){
-    currentSortCriteria = sortCriteria;
-
-    if(categoriesArray != undefined){
-        currentCategoriesArray = categoriesArray;
-    }
-
-    currentCategoriesArray = sortCategories(currentSortCriteria, currentCategoriesArray);
-
-    //Muestro las categorías ordenadas
-    showCategoriesList();
 }
 
 
@@ -83,15 +35,11 @@ function sortAndShowCategories(sortCriteria, categoriesArray){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-           sortAndShowCategories(ORDER_ASC_BY_NAME, resultObj.data);
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            
         }
+        currentProductArray = resultObj.data;
+        showProductList();
     });
-
-   
-
-    
-
-        showCategoriesList();
-    });
+});
